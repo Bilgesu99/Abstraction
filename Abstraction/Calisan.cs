830fa5226cbdee021b563a7d98802b40b5b8746f
asus <asus@DESKTOP-OTTG4TB>
2024-08-20 20:10:48 +0300

    Add project files.

---
+++ Abstraction/Calisan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+        public abstract void Gorev();
+
+    }
+
+    public class YazilimGeliştirici: Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Yazılım Geliştiricisi, yazılım geliştiriyorum.");
+        }
+    }
+
+    public class ProjeYöneticisi: Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Proje Yöneticisi, proje yönetiyorum.");
+
+        }
+    }
+
+    public class SatisTemsilcisi: Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} şirketin Satış Temsilcisi, satış yapıyorum.");
+        }
+    }
+
+
+
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using Abstraction;
+using System.Net.Sockets;
+
+Calisan yazilimci = new YazilimGeliştirici
+{
+    Ad = "aa",
+    Soyad = "bb",
+    Departman = "Yazılım Geliştirme"
+};
+yazilimci.Gorev();
+
+// Proje Yöneticisi örneği
+Calisan yonetici = new ProjeYöneticisi
+{
+    Ad = "dd",
+    Soyad = "kk",
+    Departman = "Proje Yönetimi"
+};
+yonetici.Gorev();
+
+// Satış Temsilcisi örneği
+Calisan satici = new SatisTemsilcisi
+{
+    Ad = "ff",
+    Soyad = "ss",
+    Departman = "Satış"
+};
+satici.Gorev();
